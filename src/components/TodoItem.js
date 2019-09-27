488de1cc795907edebf65266808355ab67cc573e
Youngjun Woo <youngjunwoo@Youngjuns-MacBook-Pro.local>
2019-09-27 01:42:38 -0700

    Added propTypes

---
+++ src/components/TodoItem.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+  getStyle = () => {
+    if (this.props.todo.completed) {
+      return {
+        textDecoration: 'line-through'
+      };
+    } else {
+      return {
+        textDecoration: 'none'
+      };
+    }
+  };
+  render() {
+    return (
+      <div style={this.getStyle()}>
+        <p>{this.props.todo.title}</p>
+      </div>
+    );
+  }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+};
+
+export default TodoItem;
